1c4931a138260f35c360523aaea4de792337aeff
Red Maple <M8R-558ciw@safetymail.info>
2012-12-23 23:09:40 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '8a38aecede1f3d7fbd03fddd4ee484ee91ba291bbc3bcaa9ff022a67901fa7bfbe0ecd7676387a8c6cb70fd0d8ba8958ac05badbd051bb67554bb23cd613db8f'
